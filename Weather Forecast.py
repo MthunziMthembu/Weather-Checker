9c0b566442e3607113a165929828db6f56afbc7b
Mthunzi Mthembu <168267555+MthunziMthembu@users.noreply.github.com>
2025-08-13 13:45:48 +0200

    Add files via upload

---
+++ Weather Forecast.py
@@ -0,0 +1,29 @@
+weather = input("Enter weather forecast:  ")
+if weather == "sunny":
+   print("go outside and play")
+elif  weather =="rainy":
+   print ("Stay inside and read a book")
+else:
+   print("Check the forecast again")
+
+print('-----------------------------------') 
+Day_of_week = input("Enter day of the week:    ")
+if Day_of_week == "Monday":
+   print("Start of the week")
+elif Day_of_week == "Friday":   
+   print("Almost weekend")
+else:
+   print ("Just another day")
+
+('-----------------------------------')
+Temp = int(input("Enter Temperature: "))
+if Temp > 25: 
+    print("Hot Weather")
+
+elif Temp > 15 and Temp < 25:
+   print ('Mild Weather')
+
+else :
+    print ('Cold weather')
+
+
